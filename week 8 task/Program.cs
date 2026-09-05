f2bd00495e853005db5e7de6529133fb4a3dad54
Mohammad Mobarak <mohammad2632002mobarak@gmail.com>
2026-09-05 23:40:59 +0300

    Add project files.

---
+++ week 8 task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace week_8_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello bashmohandes Ahmed , this is week 8 task !");
+        }
+    }
+}
